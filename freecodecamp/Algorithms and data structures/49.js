69ce7fdf5a85b10473609ea474bb0fa0175e2d15
Evgeny <jevgenijs240693@gmail.com>
2021-10-24 10:44:56 +0100

    freecodecamp 48-50 tasks

---
+++ freecodecamp/Algorithms and data structures/49.js
@@ -0,0 +1,12 @@
+
+var myDog = {
+// Only change code below this line
+
+    "name": "Lola",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["Sam", "Sarah"]
+
+
+// Only change code above this line
+};

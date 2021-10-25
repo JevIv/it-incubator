e036d99d26c3a1dbde315d7679a6d11623b9bd25
Evgeny <jevgenijs240693@gmail.com>
2021-10-25 13:56:36 +0300

    freecodecamp 52 task

--- freecodecamp/Algorithms and data structures/52.js
+++ freecodecamp/Algorithms and data structures/52.js
@@ -1,11 +1,11 @@
 // Setup
 var testObj = {
-    "hat": "ballcap",
-    "shirt": "jersey",
-    "shoes": "cleats"
+  12: "Namath",
+  16: "Montana",
+  19: "Unitas"
 };
 
 // Only change code below this line
 
-var hatValue = testObj.hat;      // Change this line
-var shirtValue = testObj.shirt;    // Change this line
+var playerNumber = 16;       // Change this line
+var player = testObj[playerNumber];   // Change this line

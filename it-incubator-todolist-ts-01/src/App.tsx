fe351d9de6e279cd18587f8cebf017d5bb054cbe
Evgeny <jevgenijs240693@gmail.com>
2021-11-08 18:51:13 +0000

    todolist title/tasks

--- it-incubator-todolist-ts-01/src/App.tsx
+++ it-incubator-todolist-ts-01/src/App.tsx
@@ -1,26 +1,34 @@
 import React from 'react';
 import './App.css';
+import ToDoList from "./ToDoList";
+
+export type TaskType = {
+    id: number
+    title: string
+    isDone: boolean
+}
 
 function App() {
+    //BLL: business layer
+    const todoListTitle_1: string = "1 To do list"
+    const todoListTitle_2: string = "2 To do list"
+
+    const tasks_1: Array<TaskType> = [
+        {id: 1, title: "HTML", isDone: true},
+        {id: 1, title: "CSS", isDone: true},
+        {id: 1, title: "REACT", isDone: false},
+    ]
+    const tasks_2: Array<TaskType> = [ //giving an array
+        {id: 1, title: "Meat", isDone: false},
+        {id: 1, title: "Beer", isDone: false},
+        {id: 1, title: "Milk", isDone: true},
+    ]
+
+    //UI: UI layer
     return (
         <div className="App">
-            <div>
-                <h3>What to learn</h3>
-                <div>
-                    <input/>
-                    <button>+</button>
-                </div>
-                <ul>
-                    <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
-                    <li><input type="checkbox" checked={true}/> <span>JS</span></li>
-                    <li><input type="checkbox" checked={false}/> <span>React</span></li>
-                </ul>
-                <div>
-                    <button>All</button>
-                    <button>Active</button>
-                    <button>Completed</button>
-                </div>
-            </div>
+            <ToDoList title={todoListTitle_1} tasks={tasks_1}/>
+            <ToDoList title={todoListTitle_2} tasks={tasks_2}/>
         </div>
     );
 }

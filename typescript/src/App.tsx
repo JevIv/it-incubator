8f8d32996b7d8ce7aa11257bcdde16459215bcfa
Evgeny <jevgenijs240693@gmail.com>
2021-11-11 17:44:54 +0000

    state typization

--- typescript/src/App.tsx
+++ typescript/src/App.tsx
@@ -2,20 +2,30 @@ import React from 'react';
 import './App.css';
 import Accordion from "./components/Accordion/Accordion";
 import {Rating} from "./components/Rating/Rating";
+import {BrowserRouter, Route} from "react-router-dom";
+import state from "./redux/state";
 
 function App() {
+
+    let message = state.profilePage.posts[0].message
+    let message2 = state.profilePage.posts[1].message
+
     return (
-        <div className="App">
-            <PageTitle title={"This is APP component"}/>
-            <div>This is APP component</div>
-            <input/>
-            Artcile 1
-            <Rating value={3}/>
-            Artcile 2
-            <Rating value={6}/>
-            <Accordion title={"This is Accordion title"} collapsed={false}/>
-            <Accordion title={"This is Accordion title 2"} collapsed={true}/>
-        </div>
+        <BrowserRouter>
+            <div className="App">
+                <PageTitle title={"This is APP component"}/>
+                <div>This is APP component</div>
+                <input/>
+                Artcile 1
+                <Rating value={3}/>
+                Artcile 2
+                <Rating value={6}/>
+                <Accordion title={"This is Accordion title"} collapsed={false}/>
+                <Accordion title={"This is Accordion title 2"} collapsed={true}/>
+                <Route path={"/hello"} render={ () => <HelloMessage message={message}/>}/>
+                <Route path={"/bye"} render={ () => <ByeMessage message={message2}/>}/>
+            </div>
+        </BrowserRouter>
     );
 }
 
@@ -24,7 +34,7 @@ type PageTitlePropsType = {
 }
 
 function PageTitle(props: PageTitlePropsType) {
-    return <h1>{ props.title }</h1>
+    return <h1>{props.title}</h1>
 }
 
 type MessageType = {

4c6a808207b4b79022670f79b30cb3db545c55d3
Evgeny <jevgenijs240693@gmail.com>
2021-11-12 17:18:00 +0000

    added some styling for template components

--- social_network/src/App.tsx
+++ social_network/src/App.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import './App.css';
-import Header from "./components/Header/Header";
-import Technologies from "./components/Technologies/Technologies";
 
 const App = () => {
     return (
-        <div>
-            <Header/>
-            <Technologies/>
+        <div className="app-wrapper">
+            <header className="header">
+                <img
+                    src="https://user-images.githubusercontent.com/6764957/52892445-9045cf80-3136-11e9-9d5e-a1c47e505372.png"
+                    alt=""/>
+            </header>
+            <nav className="nav">
+                <div>
+                    Profile
+                </div>
+            </nav>
+            <div className="content">
+                Main content
+            </div>
         </div>
     );
 }

b0c38c05377f6dc3d571dbae65d166226cf834c0
Evgeny <jevgenijs240693@gmail.com>
2021-11-14 11:29:15 +0000

    first homework done

--- homeworks/src/p2-homeworks/h1/Message.tsx
+++ homeworks/src/p2-homeworks/h1/Message.tsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import s from "./Message.module.css"
+import {MessageType} from "./HW1";
 
-function Message() {
+type PropsType = {
+    avatar: string
+    name: string
+    message: string
+    time: string
+}
+
+function Message(props: PropsType) {
     return (
-        <div>
-
+        <div className={s.messageBlock}>
+            <img
+                src={props.avatar}
+                //src="https://lwlies.com/wp-content/uploads/2017/04/avatar-2009.jpg"
+                alt=""/>
+            <div className={s.message}>
+                <p className={s.senderName}>{props.name}</p>
+                <p className={s.text}>{props.message}</p>
+                <time>{props.time}</time>
+            </div>
         </div>
     )
 }

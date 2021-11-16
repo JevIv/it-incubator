7c9caf2db1585de797c30b270a4308823313416c
Evgeny <jevgenijs240693@gmail.com>
2021-11-16 22:08:38 +0000

    first week done

--- todolist_dymich/src/App.tsx
+++ todolist_dymich/src/App.tsx
@@ -1,17 +1,27 @@
 import React from 'react';
 import './App.css';
-import {Todolist} from "./Todolist";
+import {TaskType, Todolist} from "./Todolist";
 
 
 function App() {
+
+    let task1: Array<TaskType> = [
+        {id: 1, title: "CSS", isDone: true},
+        {id: 2, title: "JS", isDone: true},
+        {id: 3, title: "React", isDone: false},
+        {id: 4, title: "Html", isDone: true},
+    ]
+    let task2: Array<TaskType> = [
+        {id: 1, title: "Terminator", isDone: true},
+        {id: 2, title: "Last Dance", isDone: true},
+        {id: 3, title: "XXX", isDone: false},
+        {id: 4, title: "Gentlemen of fortune", isDone: true},
+    ]
+
     return (
         <div className="App">
-            <Todolist title="What to learn"/>
-            <Todolist title="Movie"/>
-            <Todolist title="Songs"/>
-            <input type="checkbox"/>
-            <input type="date"/>
-            <input placeholder={"it-incubator"}/>
+            <Todolist title="What to learn" tasks={task1}/>
+            <Todolist title="Movie" tasks={task2}/>
         </div>
     );
 }

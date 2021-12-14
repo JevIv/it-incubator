a920162f9bb06d54b2a691ad8efdf13c319dee54
Evgeny <jevgenijs240693@gmail.com>
2021-12-14 15:38:41 +0000

    export input func as separate component

---
+++ todolist/src/components/AddItemForm.tsx
@@ -0,0 +1,44 @@
+import React, {ChangeEvent, KeyboardEvent, useState} from 'react';
+import s from "../ToDoList.module.css";
+import {Button} from "./Button";
+
+type AddItemFormPropsType = {
+    addItem: (title: string) => void
+}
+
+const AddItemForm = (props: AddItemFormPropsType) => {
+    const [error, setError] = useState(true);
+    const [title, setTitle] = useState("") //input vsegda string
+    const addHandler = () => {
+        if (title.trim() !== "") {
+            props.addItem(title.trim());
+            setTitle("");
+        } else {
+            setError(true);
+        }
+    }
+    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setTitle(e.currentTarget.value);
+        setError(false);
+
+    }
+    const onKeyPressHandler = (e: KeyboardEvent<HTMLInputElement>) => {
+        if (e.key === "Enter") {
+            addHandler();
+        }
+    }
+
+    return (
+        <div>
+            <input className={error ? s.error : ""}
+                   value={title}
+                   onChange={onChangeHandler}
+                   onKeyPress={onKeyPressHandler}/>
+            {/*<button onClick={addHandler}>+</button>*/}
+            <Button name={"+"} callback={addHandler}/>
+            {error && <div className={s.errorMessage}>Title is required</div>}
+        </div>
+    );
+};
+
+export default AddItemForm;

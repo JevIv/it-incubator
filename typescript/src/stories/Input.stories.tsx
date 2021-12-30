feb6ba88fd53f3e03033dbf4a4a8a3c8efb4ba9b
Evgeny <jevgenijs240693@gmail.com>
2021-12-30 09:34:32 +0000

    uncotr

---
+++ typescript/src/stories/Input.stories.tsx
@@ -0,0 +1,33 @@
+import React, {useRef, useState} from 'react';
+
+export default {
+  title: 'input',
+  //component: input,
+};
+
+export const UncontrolledInput = () => <input />;
+
+export const TrackValueOfUncontrolledInput = () => {
+  const [value,setValue]=useState("")
+
+  return <><input onChange={(e)=> {
+    const actualValue = e.currentTarget.value;
+    setValue(actualValue);
+  }}/> - {value}</>;
+};
+
+export const GetValueOfUncontrolledInputByButtonPress = () => {
+  const [value,setValue]=useState("");
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const save = ()=> {
+    const el = inputRef.current as HTMLInputElement;
+    setValue(el.value);
+  }
+
+
+  return <><input ref={inputRef} />  <button onClick={save}>save</button> - actual value {value}</>;
+};
+
+
+export const ControlledInputWithfixedValue = () => <input value={"it-incubator.by"}/>;

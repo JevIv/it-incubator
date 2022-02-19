d37eb617ee65ea4490afd7d1a1b711f60c7005af
Evgeny <jevgenijs240693@gmail.com>
2022-02-19 10:53:36 +0000

    added authAPI

--- social_network/src/components/Header/HeaderContainer.tsx
+++ social_network/src/components/Header/HeaderContainer.tsx
@@ -1,17 +1,17 @@
 import React from "react";
 import Header from "./Header";
-import axios from "axios";
 import {connect} from "react-redux";
 import {InitialStateType, setAuthUserData} from "../../redux/auth-reducer";
 import {AppStateType} from "../../redux/redux-store";
+import {authAPI} from "../../api/api";
 
 
 class HeaderContainer extends React.Component<HeaderPropsType> {
     componentDidMount() {
-        axios.get(`https://social-network.samuraijs.com/api/1.0/auth/me`,
-            {withCredentials: true}).then(response => {
-                if (response.data.resultCode === 0) {
-                    let {id, email, login} = response.data.data
+        authAPI.loginUser()
+            .then(data => {
+                if (data.resultCode === 0) {
+                    let {id, email, login} = data.data
                     this.props.setAuthUserData(id, email, login)
                 }
         })

0251b6f024ddf25ee71a1d773e3b284467a492de
Evgeny <jevgenijs240693@gmail.com>
2022-02-19 10:46:32 +0000

    added ProfileAPI

--- social_network/src/components/Profile/ProfileContainer.tsx
+++ social_network/src/components/Profile/ProfileContainer.tsx
@@ -2,10 +2,10 @@ import React from "react";
 import Profile from "./Profile";
 import axios from "axios";
 import {connect} from "react-redux";
-import {useParams, useMatch} from "react-router-dom"
+import {useMatch} from "react-router-dom"
 import {AppStateType} from "../../redux/redux-store";
 import {InitialStateType, ProfileType, setUserProfile} from "../../redux/profile-reducer";
-import {render} from "react-dom";
+import {profileAPI} from "../../api/api";
 
 
 type ProfileContainerPropsType = {
@@ -18,8 +18,9 @@ class ProfileContainer extends React.Component<ProfileContainerPropsType>{
         let userId = this.props.match
             ? this.props.match.params.userId
             : 1
-        axios.get(`https://social-network.samuraijs.com/api/1.0/profile/${userId}`).then(response => {
-            this.props.setUserProfile(response.data)
+        profileAPI.getUser(userId)
+            .then(data => {
+                this.props.setUserProfile(data)
         })
     }
 

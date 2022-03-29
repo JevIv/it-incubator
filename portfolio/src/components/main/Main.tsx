d75af8fed105aae5ddcb58aaf3b5de4e77462c62
Evgeny <jevgenijs240693@gmail.com>
2022-03-29 18:14:26 +0100

    started new design, started create main page styles

--- portfolio/src/components/main/Main.tsx
+++ portfolio/src/components/main/Main.tsx
@@ -7,23 +7,40 @@ export const Main = () => {
     return (
         <div className={style.mainBlock}>
             <div className={`${style.mainContainer}`}>
-                <Header/>
                 <div className={style.greetingContainer}>
                     {/*<div className={style.greeting}>*/}
                     {/*    <span>Hi There,</span>*/}
                     {/*    <h1>I'm Evgeny Ivanov,</h1>*/}
                     {/*    <p>A Frontend Developer.</p>*/}
                     {/*</div>*/}
-                    <div className={style.left}>
-                        <span className={style.greeting}>Hi There</span>
-                        <h1>I'm Evgeny </h1>
-                        <p>A Frontend </p>
+
+                    <h1>
+                        <div className={style.left}>
+                            <div className={style.titleText}>
+                                Hi,<br/>
+                                Evgeny<br/>
+                                Front-end<br/>
+                            </div>
+                        </div>
+                        <div className={style.right}>
+                            <div className={style.titleText}>
+                                I'm<br/>
+                                Ivanov<br/>
+                                Developer<br/>
+                            </div>
+                        </div>
+                    </h1>
+
+                    {/*                    <div className={style.left}>
+                        <span className={style.greeting}>Hi,</span>
+                        <h1>Evgeny </h1>
+                        <p>Front-end </p>
                     </div>
                     <div className={style.right}>
-                        <span className={style.greeting}>,</span>
-                        <h1> Ivanov,</h1>
-                        <p>Developer.</p>
-                    </div>
+                        <span className={style.greeting}>I'm</span>
+                        <h1> Ivanov</h1>
+                        <p>Developer</p>
+                    </div>*/}
                 </div>
                 {/*<div className={style.avatar}></div>*/}
             </div>
